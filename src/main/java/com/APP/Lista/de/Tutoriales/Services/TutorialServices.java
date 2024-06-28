9c7a331b71d32eb47ee8f710036c514bd0378a90
Marta Gonzalez Morales <pythonisamgm@gmail.com>
2024-06-28 13:51:02 +0200

    add interface class (ITutorialRepository) extending form CrudRepository and implement some crud methods like create, read and delete tutorials

--- src/main/java/com/APP/Lista/de/Tutoriales/Services/TutorialServices.java
+++ src/main/java/com/APP/Lista/de/Tutoriales/Services/TutorialServices.java
@@ -1,4 +1,31 @@
 package com.APP.Lista.de.Tutoriales.Services;
 
+
+import com.APP.Lista.de.Tutoriales.Modules.Tutorial;
+import com.APP.Lista.de.Tutoriales.Repositories.ITutorialRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
 public class TutorialServices {
+
+    @Autowired
+    ITutorialRepository iTutorialRepository;
+
+    public ArrayList<Tutorial> getAllTutorials(){
+        return (ArrayList<Tutorial>) iTutorialRepository.findAll();
+    }
+    public Tutorial createTutorial (Tutorial newTutorial){
+        return iTutorialRepository.save(newTutorial);
+    }
+    public void deleteTutorial(Tutorial newTutorial){
+        iTutorialRepository.delete(newTutorial);
+    }
+    public void deleteAllTutorials(ArrayList<Tutorial> tutorials){
+        iTutorialRepository.deleteAll(tutorials);
+    }
+
+
 }
