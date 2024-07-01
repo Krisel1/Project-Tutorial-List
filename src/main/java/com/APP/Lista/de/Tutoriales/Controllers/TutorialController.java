1605dd07806740517356f5605cdb49cc42044a4e
MiguelAngelGSG <ma.gsg.555@gmail.com>
2024-07-01 10:20:51 +0200

    save

--- src/main/java/com/APP/Lista/de/Tutoriales/Controllers/TutorialController.java
+++ src/main/java/com/APP/Lista/de/Tutoriales/Controllers/TutorialController.java
@@ -1,4 +1,27 @@
 package com.APP.Lista.de.Tutoriales.Controllers;
 
+
+import com.APP.Lista.de.Tutoriales.Modules.Tutorial;
+import com.APP.Lista.de.Tutoriales.Services.TutorialServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+//localhost:8080
+
+@RestController
+@RequestMapping("/")
+@CrossOrigin(origins = "*")
 public class TutorialController {
+
+
+    @GetMapping(path = "/tutorial")
+        public ArrayList<Tutorial> getAllTutorial() {
+
+        //hacer algo para ir a la DDBB y traer los tutoriales
+        return ArrayList<Tutorial>;
+    }
+
+
 }
