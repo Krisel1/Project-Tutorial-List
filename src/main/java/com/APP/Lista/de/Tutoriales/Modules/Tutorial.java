fca79ca72665792e256e4c027f6a6723c5749a97
Krisel1 <kriselur@gmail.com>
2024-07-01 13:26:39 +0200

    Code Models

--- src/main/java/com/APP/Lista/de/Tutoriales/Modules/Tutorial.java
+++ src/main/java/com/APP/Lista/de/Tutoriales/Modules/Tutorial.java
@@ -1,4 +1,31 @@
 package com.APP.Lista.de.Tutoriales.Modules;
 
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "tutorials")
 public class Tutorial {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(length = 2000)
+    private String description;
+
+    public Tutorial() {
+    }
+
+    public Tutorial(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
 }
