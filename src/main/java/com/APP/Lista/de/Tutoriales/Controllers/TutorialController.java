f0f14219994ccad52112a6a6d4cd5d6c679e3b56
MiguelAngelGSG <ma.gsg.555@gmail.com>
2024-07-02 11:22:01 +0200

    save

--- src/main/java/com/APP/Lista/de/Tutoriales/Controllers/TutorialController.java
+++ src/main/java/com/APP/Lista/de/Tutoriales/Controllers/TutorialController.java
@@ -16,8 +16,28 @@ public class TutorialController {
     @Autowired
     TutorialServices tutorialServices;
 
-    @GetMapping(path = "/tutorial")
+    @GetMapping(path = "/tutorials")
     public ArrayList<Tutorial> getAllTutorials() {
         return tutorialServices.getAllTutorials();
+    }
+
+    @GetMapping(path = "/tutorial/{id}")
+    public Optional<Tutorial> getTutorialById(@PathVariable("id")int id){
+        return tutorialServices.getTutorialById(id);
+    }
+
+    @PostMapping(path = "/tutorials")
+    public Tutorial createTutorial(@RequestBody Tutorial newTutorial) {
+        return tutorialServices.createTutorial();
+    }
+
+    @PutMapping(path = "/tutorials")
+    public Tutorial updateTutorial(@RequestBody Tutorial tutorial) {
+        return tutorialServices.updateTutorial();
+    }
+
+    @DeleteMapping(path = "/tutorials")
+    public void deleteTutorial(@RequestBody Tutorial newTutorial) {
+        return tutorialServices.deleteTutorial();
     }
 }
